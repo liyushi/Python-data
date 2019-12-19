2efe03d478c40f1635a406e376e807eaeb4c25c3
liyushi <1932995905@qq.com>
2019-12-19 16:57:09 +0800

    false

--- Hello.py
+++ Hello.py
@@ -1,2 +1,16 @@
-print("hello world")
-print("adsvd")
+print("Please enter the weight and the height, must the order")
+weight,height = eval(input())
+BMI = weight/(height*height)
+if BMI < 18.5:
+    print("Your BMI is"+ BMI)
+    print("too thin")
+elif 18.5 < BMI < 23.9:
+    print("Your BMI is"+ BMI)
+    print("normal")
+elif 24 < BMI < 27.9:
+     print("Your BMI is"+ BMI)
+     print("overweight")
+elif BMI>28:
+    print("Your BMI is" + BMI)
+    print("fat")
+
