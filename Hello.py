19ca93d9fbe1ba41df990e591db0f61a54277daf
liyushi <1932995905@qq.com>
2019-12-25 10:36:03 +0800

    interesting

--- Hello.py
+++ Hello.py
@@ -1,18 +1,12 @@
 print("Please enter the weight and the height, must the order")
 weight,height = eval(input())
 BMI = weight/(height*height)
+print("Your BMI is {0:.1f}".format(BMI))
 if BMI < 18.5:
-    print("Your BMI is"+ BMI)
     print("too thin")
-elif 18.5 < BMI < 23.9:
-    print("Your BMI is"+ BMI)
+elif BMI < 24:
     print("normal")
-elif 24 < BMI < 27.9:
-     print("Your BMI is"+ BMI)
-     print("overweight")
-elif BMI>28:
-    print("Your BMI is" + BMI)
+elif BMI < 28:
+    print("overweighht")
+else:
     print("fat")
-
-
-what ? false
