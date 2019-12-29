a9b6828af6d1c1232bc15024e38bf2ededa6beda
liyushi <1932995905@qq.com>
2019-12-29 16:52:34 +0800

    wrong

--- 2.Test.py
+++ 2.Test.py
@@ -1,3 +1,8 @@
-for F in range(0,301,20):
-    c = 5/9 *(F-32)
-    print("{} F={:.0f}c".format(F,c))
+F = input("The number enter:")
+while F != 1:
+    if F%2 == 0:
+        print("{}/2={}".format(F,F//2))
+        F//=2
+    else:
+        print("{}*3+1={}".format(F,3*F+1))
+        F=3*F+1
