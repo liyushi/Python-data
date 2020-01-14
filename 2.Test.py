1cb0cb05546f3feb3ef3d1dfd5217f7306ca5999
liyushi <1932995905@qq.com>
2020-01-14 17:27:57 +0800

    emm

--- 2.Test.py
+++ 2.Test.py
@@ -1,8 +1,6 @@
-F = input("The number enter:")
-while F != 1:
-    if F%2 == 0:
-        print("{}/2={}".format(F,F//2))
-        F//=2
-    else:
-        print("{}*3+1={}".format(F,3*F+1))
-        F=3*F+1
+a=int(input('a='))
+b=int(input('b='))
+
+print('%d + %d = %d' %(a,b,a+b))
+print('%d - %d = %d' %(a,b,a-b))
+print('%d * %d = %d' %(a,b,a*b))
