f0d8295751021c355c51b6a924ad4b82f05c55ed
liyushi <1932995905@qq.com>
2020-04-27 15:44:01 +0800

    Turple

--- 7.Circule.py
+++ 7.Circule.py
@@ -25,3 +25,7 @@ print('Total %d ' % counter)
 if counter > 7:
     print('foolish')
 '''
+for i in range(1,10):
+    for j in range(1,i+1):
+        print('%d*%d=%d'%(i,j,i*j),end='\t')
+    print()
