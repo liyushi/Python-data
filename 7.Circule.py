e7dbd660635000ee644bfb12bde6713989b3c918
liyushi <1932995905@qq.com>
2020-04-27 18:55:50 +0800

    Turple

--- 7.Circule.py
+++ 7.Circule.py
@@ -59,14 +59,17 @@ for factor in range(x, 0, -1):
         print('%d和%d的最大公约数是%d' % (x, y, factor))
         print('%d和%d的最小公倍数是%d' % (x, y, x * y // factor))
         break
+
+
 '''
 row = int(input('请输入行数: '))
 for i in range(row):
-    for _ in range(i + 1):
-        print('*', end='')
+    for j in range(i + 1):
+        print('*',end='')
     print()
+'''
 
-
+row = int(input('请输入行数: '))
 for i in range(row):
     for j in range(row):
         if j < row - i - 1:
@@ -74,10 +77,19 @@ for i in range(row):
         else:
             print('*', end='')
     print()
-
+'''
+'''
 for i in range(row):
     for _ in range(row - i - 1):
         print(' ', end='')
     for _ in range(2 * i + 1):
         print('*', end='')
     print()
+'''
+
+'''
+
+
+'''
+里面一些代码还是不是很清楚
+'''
