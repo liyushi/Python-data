2ac01b0bcb5bce6f40c2df9ab9bfc4e42f48014d
liyushi <1932995905@qq.com>
2020-05-04 21:16:08 +0800

    Turple

--- test.py
+++ test.py
@@ -1,19 +1,16 @@
-from module1 import foo
-foo()
-from module2 import foo
-foo()
+import os
+import time
 
-'''
-需要说明的是，如果我们导入的模块除了定义函数之外还中有可以执行代码，那么Python解释器在导入这个模块时就会执行这些代码，
-事实上我们可能并不希望如此，因此如果我们在模块中编写了执行代码，最好是将这些执行代码放入如下所示的条件中，
-这样的话除非直接运行该模块，if条件下的这些代码是不会执行的，因为只有直接执行的模块的名字才是"__main__"
-'''
-import module3
-bar()
 
 def main():
-    # Todo: Add your code here
-    pass
+    content = '北京欢迎你为你开天辟地…………'
+    while True:
+        # 清理屏幕上的输出
+        os.system('cls')  # os.system('clear')
+        print(content)
+        # 休眠200毫秒
+        time.sleep(0.2)
+        content = content[1:] + content[0]
 
 
 if __name__ == '__main__':
