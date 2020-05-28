233a838688dc0722917e0c8ca9291fd2550ceede
liyushi <1932995905@qq.com>
2020-05-28 16:47:24 +0800

    Turple

---
+++ Zookeeper/Basic.py
@@ -0,0 +1,18 @@
+Firstname = input("Please enter the first name:")
+print("Hello",Firstname)
+
+Firstname = input("Please enter the first name:")
+Surname = input("Please enter the Surname:")
+print("Hello",Firstname,Surname)
+
+print("What do you call a bear with no teeth?\nA gummy bear!")
+
+a = int(input("Please enter the number a:"))
+b = int(input("Please enter the number b:"))
+answer = a + b
+print("The total is:",answer)
+
+a= int(input("Please enter three number a:"))
+b= int(input("Please enter three number b:"))
+c= int(input("Please enter three number c:"))
+print("The answer is:",(a+b)*c)
---
+++ Zookeeper/Zoon.py
@@ -0,0 +1,3 @@
+SlicesStart = input("Please enter the Started Slices:")
+SlicesEnd = input("Please enter the End Slices")
+print("They have eaten:",(SlicesStart-SlicesEnd))
