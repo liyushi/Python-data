a58381df788c37db97c1f9a067e347b63568ff9b
liyushi <1932995905@qq.com>
2020-06-05 17:02:05 +0800

    Turple

--- Zookeeper/Basic.py
+++ Zookeeper/Basic.py
@@ -16,3 +16,15 @@ a= int(input("Please enter three number a:"))
 b= int(input("Please enter three number b:"))
 c= int(input("Please enter three number c:"))
 print("The answer is:",(a+b)*c)
+
+SlicesStart = int(input("Please enter the Started Slices:"))
+SlicesEnd = int(input("Please enter the End Slices"))
+print("They have eaten:",(SlicesStart-SlicesEnd))
+
+age = int(input("Please input your age:"))
+Name = input("Please input your Name:")
+print(Name,"next birthday you will be",(age+1))
+
+Price = float(input("Please input the bill:"))
+Dinner = int(input("How many dinners you are"))
+print("The bill you must pay:",Price/Dinner)
--- Zookeeper/Zoon.py
+++
@@ -1,3 +0,0 @@
-SlicesStart = input("Please enter the Started Slices:")
-SlicesEnd = input("Please enter the End Slices")
-print("They have eaten:",(SlicesStart-SlicesEnd))
